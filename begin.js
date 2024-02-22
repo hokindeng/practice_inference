0fba54fa44e245f625232beadc1265d78730c365
XueqianDeng <denghokin@gmail.com>
2024-02-22 16:26:17 -0500

    add hammer

--- begin.js
+++ begin.js
@@ -4,3 +4,44 @@ function redirectToSecondPage() {
     // Redirect to second_page.html
     window.location.href = "second_page.html";
 }
+
+// generate local mapping
+num_symb = 5
+key_list = ["h", "u", "i", "l",'space'];
+x_symb = [0, 1, 2, 3, 4];  // 5 stimuli indexed as 0:4; response key is always indexed as 0:4
+symb_perm = permute(x_symb); // permutation
+symb_map_rnd = Math.floor(rng * symb_perm.length) // choose a random permutation from symb_perm
+symb_map_ind = symb_perm[symb_map_rnd];  // re-arrange symb index based on selected permutation
+for (var i = 0, _pj_a = num_symb; (i < _pj_a); i += 1) {
+symb_map.push(symb[symb_map_ind[i]]);
+}
+// this re-arranged symb: [s2, 24, 23, s1, s7 ...]; response key index does not change still 1:8; so mapping changes
+// so now, symb_map is a list of only 8 elements;
+// form a list of 96 elements 8*16 and randomize it; this would be the sequential of trial of a block
+
+// to determine whether the response is correct for each trial
+actual_choice = key_list.indexOf(actual_press);
+propose_choice = symb_map_push(i)
+//check if actual_choice == propose_choice
+
+function permute(permutation) {
+  var length = permutation.length,
+      result = [permutation.slice()],
+      c = new Array(length).fill(0),
+      i = 1, k, p;
+  while (i < length) {
+    if (c[i] < i) {
+      k = i % 2 && c[i];
+      p = permutation[i];
+      permutation[i] = permutation[k];
+      permutation[k] = p;
+      ++c[i];
+      i = 1;
+      result.push(permutation.slice());
+    } else {
+      c[i] = 0;
+      ++i;
+    }
+  }
+  return result;
+}
--- learning.js
+++ learning.js
@@ -9,14 +9,18 @@ let innerHtml = ` <img src="image/img1.jpg"/> <div class="mouse"><img src="image
  style="transform: translateY(100%) !important;" class="mouse_img"/></div> <div class="hammer"><img src="image/hammer.png" 
  style="transform: translateY(100%) !important;" class="hammer"/></div>`
 
-
 let GameIndex = ''      // 当前地鼠处于哪个位置
 let fraction = 0        // 当前得分
 let timeNumber = 0      // 当前时间
 let timer1 = null       // 第一个计时器
 let timer2 = null       // 第一个计时器
 
+function one_trial(){
+
+}
+
 timer()
+
 
 function timer() {
     timer1 = setInterval(function () {
