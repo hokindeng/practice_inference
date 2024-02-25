d4d78026d257862f6dc8ab02d4b9ebfce857731a
XueqianDeng <denghokin@gmail.com>
2024-02-25 16:13:45 -0500

    hide innerhtml

--- total.js
+++ total.js
@@ -1,29 +1,19 @@
 // script.js
 
 let start_game = document.querySelector(".start_game")
-let music1 = document.querySelector(".music1")
-let music2 = document.querySelector(".music2")
-let music3 = document.querySelector(".music3")
-let trial = document.querySelector(".trial")
-let end_game = document.querySelector(".end_game")
-// Begin
+let background_music = document.querySelector(".music1")
+
+
+// let innerHtml = ` <img src="image/img1.jpg"/> <div class="mouse"><img src="image/mouse.png"
+// style="transform: translateY(100%) !important;" class="mouse_img"/></div> <div class="hammer"><img src="image/hammer.png"
+// style="transform: translateY(100%) !important;" class="hammer"/></div>`
 
 function redirectToSecondPage() {
     start_game.style.top = '-100%'
-    music1.volume = 0.1
-    music1.play()
-    let trial = document.querySelector(".trial");
-    if (trial) {
-        trial.textContent = "20"; // This will change the content of the .trial element to "20"
-    }
+    background_music.volume = 0.1
+    background_music.play()
     one_trial()
 }
-
-// Learning
-
-let innerHtml = ` <img src="image/img1.jpg"/> <div class="mouse"><img src="image/mouse.png" 
- style="transform: translateY(100%) !important;" class="mouse_img"/></div> <div class="hammer"><img src="image/hammer.png" 
- style="transform: translateY(100%) !important;" class="hammer"/></div>`
 
 let GameIndex = ''      // 当前地鼠处于哪个位置
 let fraction = 9       // 当前得分
