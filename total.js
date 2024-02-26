9ca4a696301b0a83d7ef42ee58f82e1906743506
XueqianDeng <denghokin@gmail.com>
2024-02-26 01:43:40 -0500

    s

--- total.js
+++ total.js
@@ -5,7 +5,7 @@ let background_music = document.querySelector(".music1")
 
 
 let render_hammer = `<img src="image/img1.jpg"/> <div class="hammer"><img src="image/hammer.png" 
-style="transform: translateX(-80px) translateY(-100px) scale(2) !important;" class="hammer"/></div>`;
+style="transform: translateX(-70px) translateY(-100px) scale(2) !important;" class="hammer"/></div>`;
 
 function redirectToSecondPage() {
     start_game.style.top = '-100%'
