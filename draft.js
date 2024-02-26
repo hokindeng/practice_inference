dd490edaa3d1e76ca7a60a1abae3fb81fb2c6313
XueqianDeng <denghokin@gmail.com>
2024-02-26 02:09:56 -0500

    add draft

---
+++ draft.js
@@ -0,0 +1,3 @@
+let render_hammer = `<img src="image/img1.jpg"/> <div class="hammer"><img src="image/hammer.png" 
+style="transform: translateX(-70px) translateY(-100px) scale(2) !important;" class="hammer"/>
+<img src="image/empty_circle.png" style="transform:  translateX(-30px) translateY(-180px) scale(.6)"/></div>`;
