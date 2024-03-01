cf8761a9109681baa96bb0d016a458f74e8338df
XueqianDeng <denghokin@gmail.com>
2024-02-29 20:25:12 -0800

    hello

--- total.js
+++ total.js
@@ -6,12 +6,14 @@ let background_music = document.querySelector(".music1")
 
 let render_hammer = `<img src="image/img1.jpg"/> <div class="hammer"><img src="image/hammer.png" 
 style="transform: translateX(-70px) translateY(-100px) scale(2) !important;" class="hammer"/></div>`;
+let blue_context = '<div class ="context_cue_blue'></div>'
 
 function redirectToSecondPage() {
     start_game.style.top = '-100%'
     background_music.volume = 0.1
     background_music.play()
     one_trial()
+    context_change()
 }
 
 let GameIndex = ''      // 当前地鼠处于哪个位置
@@ -23,4 +25,8 @@ let timer2 = null       // 第一个计时器
 function one_trial(){
     GameIndex = Math.floor(Math.random() * 9)
     document.querySelector('.game' + GameIndex).innerHTML = render_hammer
+}
+
+function context_change(){
+    document.querySelector('.context_cue').innerHTML = blue_context
 }
