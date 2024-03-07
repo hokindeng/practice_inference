86d50af8a527ee4fe2b617179bf4785edcbacb45
XueqianDeng <denghokin@gmail.com>
2024-03-06 20:25:10 -0800

    all context change javascript done

--- total.js
+++ total.js
@@ -5,14 +5,13 @@ let background_music = document.querySelector(".music1")
 
 let render_hammer = `<img src="image/img1.jpg"/> <div class="hammer"><img src="image/hammer.png" 
 style="transform: translateX(-70px) translateY(-100px) scale(2) !important;" class="hammer"/></div>`;
-let blue_context = '<div class="context_cue_blue"></div>';
 
 function redirectToSecondPage() {
     start_game.style.top = '-100%'
     background_music.volume = 0.1
     background_music.play()
     one_trial()
-    context_change()
+    context_change_into_rose()
 }
 
 let GameIndex = ''      // 当前地鼠处于哪个位置
@@ -26,10 +25,26 @@ function one_trial(){
     document.querySelector('.game' + GameIndex).innerHTML = render_hammer
 }
 
-function context_change(){
+function context_change_into_rose(){
     let element = document.querySelector('.context_cue');
     if (element) {
         element.classList.remove('context_cue');
         element.classList.add('context_cue_rose');
+    }
+}
+
+function context_change_into_blue(){
+    let element = document.querySelector('.context_cue');
+    if (element) {
+        element.classList.remove('context_cue');
+        element.classList.add('context_cue_blue');
+    }
+}
+
+function context_change_into_purple(){
+    let element = document.querySelector('.context_cue');
+    if (element) {
+        element.classList.remove('context_cue');
+        element.classList.add('context_cue_purple');
     }
 }
