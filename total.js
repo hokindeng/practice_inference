f34fdfd2b4ec4c9e7d16985ec577dcbe6c4c6b06
XueqianDeng <denghokin@gmail.com>
2024-03-06 20:05:55 -0800

    remove ori contexxt

--- total.js
+++ total.js
@@ -3,7 +3,6 @@
 let start_game = document.querySelector(".start_game")
 let background_music = document.querySelector(".music1")
 
-
 let render_hammer = `<img src="image/img1.jpg"/> <div class="hammer"><img src="image/hammer.png" 
 style="transform: translateX(-70px) translateY(-100px) scale(2) !important;" class="hammer"/></div>`;
 let blue_context = '<div class="context_cue_blue"></div>';
