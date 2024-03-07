88d234be15a9dda6d493146cf7280811c6983d86
XueqianDeng <denghokin@gmail.com>
2024-03-06 19:10:36 -0800

    fix a mistake in git blue

--- total.js
+++ total.js
@@ -6,7 +6,7 @@ let background_music = document.querySelector(".music1")
 
 let render_hammer = `<img src="image/img1.jpg"/> <div class="hammer"><img src="image/hammer.png" 
 style="transform: translateX(-70px) translateY(-100px) scale(2) !important;" class="hammer"/></div>`;
-let blue_context = '<div class ="context_cue_blue'></div>'
+let blue_context = '<div class="context_cue_blue"></div>';
 
 function redirectToSecondPage() {
     start_game.style.top = '-100%'
