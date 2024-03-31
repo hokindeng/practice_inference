9e73485fcbe82be978ecac1c48e32e9200847695
XueqianDeng <denghokin@gmail.com>
2024-03-31 02:21:58 -0400

    add mixture training block

--- total.js
+++ total.js
@@ -85,6 +85,7 @@ async function run_experiment() {
     //await training_mapping_1() // rose
     //await training_mapping_2() // blue
     //await training_mapping_3() // purple
+    await mixture_training_mapping()
     await entering_inference()
     await training_inference()
     await downloadArrayAsFile(data, 'data')
