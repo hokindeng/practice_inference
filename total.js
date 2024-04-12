dfbff5a9023a71136a343c325024467df47b3be4
XueqianDeng <denghokin@gmail.com>
2024-04-12 01:15:26 -0400

    pilot

--- total.js
+++ total.js
@@ -83,10 +83,10 @@ function redirectToSecondPage() {
 }
 
 async function run_experiment() {
-    await training_mapping_1() // rose
-    await training_mapping_2() // blue
-    await training_mapping_3() // purple
-    await mixture_training_mapping()
+    //await training_mapping_1() // rose
+    //await training_mapping_2() // blue
+    //await training_mapping_3() // purple
+    //await mixture_training_mapping()
     await entering_inference()
     await training_inference()
     await downloadArrayAsFile(data, 'data')
